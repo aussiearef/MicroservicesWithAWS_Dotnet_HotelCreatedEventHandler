bcdaea2157fad21bfcc02866a4d2c086894fd60d
aussiearef <arefkr@gmail.com>
2023-04-10 18:48:36 +1000

    Added code for Idempotent Consistency

---
+++ EventHandlers/HotelCreatedEventHandler/HotelCreatedEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.SNSEvents;
+
+namespace HotelCreatedEventHandler;
+
+public class HotelCreatedEventHandler
+{
+    public async Task Handler(SNSEvent snsEvent)
+    {
+        var dbClient = new AmazonDynamoDBClient();
+        var table = Table.LoadTable(dbClient, "hotel-created-event-ids");
+
+        foreach (var eventRecord in snsEvent.Records)
+        {
+            var eventId = eventRecord.Sns.MessageId;
+            var foundItem = await table.GetItemAsync(eventId);
+            if (foundItem == null)
+                await table.PutItemAsync(new Document
+                {
+                    ["foundItem"] = eventId
+                });
+
+            // Process Event Here
+        }
+    }
+}
